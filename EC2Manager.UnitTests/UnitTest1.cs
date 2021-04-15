74e3cb09d54259c17230d5303f8ec1aa18190dde
Lorcan Chinnock <ljchinnock@gmail.com>
2021-04-15 12:01:14 +0100

    Add unit test project (No actual tests yet)

---
+++ EC2Manager.UnitTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace EC2Manager.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
